c7a19285085421a5616cd2f5699a474fdd7bcac0
Ruthle55Owo <thaddeusteo6000@gmail.com>
2021-11-18 11:37:36 +0800

    Completed bs

---
+++ setup.py
@@ -0,0 +1,9 @@
+from distutils.core import setup
+
+setup(name='Game Data',
+      version='0.01',
+      description='Get data from games where the api is available',
+      author='Ruthle55',
+      author_email='ruthle55.enquiries@gmail.com',
+      url='https://www.youtube.com/channel/UCl38psfQlmcrt5r1siyCQCA',
+     )
